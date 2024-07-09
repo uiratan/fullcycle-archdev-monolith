2f81bc8c2f032aff4c19e108a88210152e9b1257
Uiratan Cavalcante <uiratan@gmail.com>
2024-07-09 17:10:04 -0300

    Add Product usecase TDD

---
+++ src/modules/product-adm/usecase/add-product/add-product.usecase.spec.ts
@@ -0,0 +1,41 @@
+import AddProductUsecase from "./add-product.usecase";
+
+const MockRepository = () => {
+  return {
+    add: jest.fn(),
+    find: jest.fn(),
+  }
+}
+
+describe("Add Product usecase unit test", () => {
+  
+  it("should add a product", async() => {
+
+    // repositorio
+    const productRepository = MockRepository();
+    
+    // usecase
+    const usecase = new AddProductUsecase(productRepository);
+
+    // input
+    const input = {
+      name: "Product 1",
+      description: "Product 1 description",
+      purchasePrice: 100,
+      stock: 10
+    }
+
+    
+    // output
+    const result = await usecase.execute(input);  
+
+    expect(productRepository.add).toHaveBeenCalled();
+    expect(result.id).toBeDefined;
+    expect(result.name).toBe(input.name);
+    expect(result.description).toBe(input.description);
+    expect(result.purchasePrice).toBe(input.purchasePrice);
+    expect(result.stock).toBe(input.stock);
+    
+  });
+
+})
