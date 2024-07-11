e5afbe636154ed1e6b6783d743e535568ead088f
Uiratan Cavalcante <uiratan@gmail.com>
2024-07-11 19:15:44 -0300

    client-admin add client usecase

---
+++ src/modules/client-adm/usecase/add-client/add-client.usecase.spec.ts
@@ -0,0 +1,36 @@
+import AddClientUsecase from "./add-client.usecase";
+
+const MockRepository = () => {
+  return {
+    add: jest.fn(), // nao retorna nada pq o retorno de ClientGateway é void
+    find: jest.fn(), // nao retorna nada agora
+  };
+};
+
+describe("Add Client usecase unit test", () => {
+
+  it("should add a client", async() => {
+
+    const clientRepository = MockRepository();
+    const usecase = new AddClientUsecase(clientRepository); // ainda nao existe, teste vai falhar
+    const input = {
+      // id: "1",
+      name: "Client 1",
+      email: "a@a.com",
+      address: "address 1",
+    };
+
+    const result = await usecase.execute(input);
+
+    console.log(result);
+    
+
+    expect(clientRepository.add).toHaveBeenCalled();
+    expect(result.id).toBeDefined();
+    expect(result.name).toEqual(input.name);
+    expect(result.email).toEqual(input.email);
+    expect(result.address).toEqual(input.address);
+
+  });
+
+});
