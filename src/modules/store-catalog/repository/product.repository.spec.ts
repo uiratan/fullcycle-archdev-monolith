a913c2e35b065dd1cb09dc815b045d8b901ee785
Uiratan Cavalcante <uiratan@gmail.com>
2024-07-11 17:07:13 -0300

    store-catalog Product repository

--- src/modules/store-catalog/repository/product.repository.spec.ts
+++ src/modules/store-catalog/repository/product.repository.spec.ts
@@ -1,5 +1,6 @@
 import { Sequelize } from "sequelize-typescript";
 import ProductModel from "./product.model";
+import ProductRepository from "./product.repository";
 
 describe("Product repository unit test", () => {
 
@@ -40,11 +41,11 @@ describe("Product repository unit test", () => {
     const products = await productRepository.findAll();
 
     expect(products).toHaveLength(2);
-    expect(products[0].id).toEqual("1");
+    expect(products[0].id.id).toEqual("1");
     expect(products[0].name).toEqual("Product 1");
     expect(products[0].description).toEqual("Product 1 description");
     expect(products[0].salesPrice).toEqual(100);
-    expect(products[1].id).toEqual("2");
+    expect(products[1].id.id).toEqual("2");
     expect(products[1].name).toEqual("Product 2");
     expect(products[1].description).toEqual("Product 2 description");
     expect(products[1].salesPrice).toEqual(200);
