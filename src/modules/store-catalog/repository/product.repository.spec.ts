f9cdd54931e813158dac0a08af15e4c5840057c8
Uiratan Cavalcante <uiratan@gmail.com>
2024-07-11 17:29:24 -0300

    store-catalog find Product repository

--- src/modules/store-catalog/repository/product.repository.spec.ts
+++ src/modules/store-catalog/repository/product.repository.spec.ts
@@ -51,5 +51,22 @@ describe("Product repository unit test", () => {
     expect(products[1].salesPrice).toEqual(200);
   });
 
+  it("should find a product", async () => {
+    await ProductModel.create({
+      id: "1",
+      name: "Product 1",
+      description: "Product 1 description",
+      salesPrice: 100
+    });
+
+    const productRepository = new ProductRepository();
+    const product = await productRepository.find("1");
+
+    expect(product.id.id).toEqual("1");
+    expect(product.name).toEqual("Product 1");
+    expect(product.description).toEqual("Product 1 description");
+    expect(product.salesPrice).toEqual(100);
+  });
+
 });
 
