9b98322c735d8080a768e47fb101c03aba76f9dd
Uiratan Cavalcante <uiratan@gmail.com>
2024-07-12 10:58:34 -0300

    payment domain layer

---
+++ src/modules/payment/domain/transaction.ts
@@ -0,0 +1,64 @@
+import AggregateRoot from "../../@shared/domain/entity/aggregate-root.interface";
+import BaseEntity from "../../@shared/domain/entity/base.entity";
+import Id from "../../@shared/domain/value-object/id.value-object";
+
+type TransactionProps = {
+  id?: Id;
+  amount: number;
+  orderId: string;
+  status?: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export class Transaction extends BaseEntity implements AggregateRoot{
+
+  private _amount: number;
+  private _orderId: string;
+  private _status: string;
+
+  constructor(props: TransactionProps) {
+    super(props.id, props.createdAt, props.updatedAt);
+    this._amount = props.amount;
+    this._orderId = props.orderId;
+    this._status = props.status || "pending";
+    this.validate();
+  }
+
+  validate(): void {
+    if (this._amount <= 0) {
+      throw new Error("Amount must be greater than 0");
+    }
+  }
+
+  process(): void {
+    if (this._amount >= 100) {
+      this.approve();
+    } else {
+      this.decline();
+    }
+  }
+
+  approve(): void {
+    this._status = "approved";
+  }
+
+  decline(): void {
+    this._status = "declined";
+  }
+
+  get amount(): number {
+    return this._amount;
+  }
+
+  get orderId(): string {
+    return this._orderId;
+  }
+
+  get status(): string {
+    return this._status;
+  }
+
+
+
+}
